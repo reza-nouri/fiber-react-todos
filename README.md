b47873094ecc2df5141b9d83093f916d6914266e
Reza Nouri <reza.nouri1993@outlook.com>
2022-11-17 14:46:02 +0330

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fiber-react-todo
+
+<h3 align="center">
+اپلیکشن فهرست کار با ری اکت و فایبر
+</h3>
+
+<div align="right" dir="rtl">
+</div>
---
+++ react-todos/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <React.Fragment>
+            <h1>Todo App</h1>
+        </React.Fragment>
+    )
+}
+
+export default App;
